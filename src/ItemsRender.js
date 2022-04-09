440761339e021033802e418fde033a1c97435b49
Peter <fpv5gr@virginia.edu>
2022-04-09 18:32:44 -0400

    fixed prices

--- src/ItemsRender.js
+++ src/ItemsRender.js
@@ -10,10 +10,11 @@ const ItemsRender = (props) => {
     };
     return (
         // <p className="item"> {itemsFiltered.map((item) => item.food, item.price)} </p>
+        // How do I loop the code below? It also doesn't scale to longer lists
         <>
         <p className="item"> {itemsFiltered[0] ? itemsFiltered[0].food : ''} (${itemsFiltered[0] ? itemsFiltered[0].price : ''}) </p>
-        <p className="item"> {itemsFiltered[1] ? itemsFiltered[1].food : ''} (${itemsFiltered[1] ? itemsFiltered[0].price : ''}) </p>
-        <p className="item"> {itemsFiltered[2] ? itemsFiltered[2].food : ''} (${itemsFiltered[2] ? itemsFiltered[0].price : ''}) </p>
+        <p className="item"> {itemsFiltered[1] ? itemsFiltered[1].food : ''} (${itemsFiltered[1] ? itemsFiltered[1].price : ''}) </p>
+        <p className="item"> {itemsFiltered[2] ? itemsFiltered[2].food : ''} (${itemsFiltered[2] ? itemsFiltered[2].price : ''}) </p>
         </>
     )
 };
